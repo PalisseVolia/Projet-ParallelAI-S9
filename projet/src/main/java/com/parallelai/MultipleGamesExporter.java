9080ff079e6b385149584fbf3d38b4a522d59124
watson67 <thibaut.waechter@gmail.com>
2024-12-03 10:54:35 +0100

    aaaa

--- projet/src/main/java/com/parallelai/MultipleGamesExporter.java
+++ projet/src/main/java/com/parallelai/MultipleGamesExporter.java
@@ -1,5 +1,11 @@
 package com.parallelai;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * Gestionnaire d'export de données pour l'apprentissage automatique d'Othello.
  * Cette classe permet de:
@@ -9,7 +15,6 @@ package com.parallelai;
  */
 public class MultipleGamesExporter {
     /** Chemin du fichier CSV de sortie */
-    @SuppressWarnings("unused")
     private final String outputPath;
     
     /** Nombre total de parties à jouer */
@@ -89,12 +94,116 @@ public class MultipleGamesExporter {
     }
 
     /**
+     * Représente une situation de jeu unique
+     */
+    private static class BoardSituation {
+        private final String boardState;
+        private int winCount;
+        private int totalCount;
+
+        public BoardSituation(String boardState) {
+            this.boardState = boardState;
+            this.winCount = 0;
+            this.totalCount = 0;
+        }
+
+        public void addOccurrence(boolean isWinning) {
+            if (isWinning) winCount++;
+            totalCount++;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            BoardSituation that = (BoardSituation) o;
+            return Objects.equals(boardState, that.boardState);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(boardState);
+        }
+    }
+
+    /**
+     * Crée un dataset sans doublons en comptant les occurrences de chaque situation.
+     */
+    public void playAndExportUniqueGames() {
+        System.out.println("Début de l'export de " + nbParties + " parties avec dédoublonnage...");
+        Map<String, BoardSituation> situations = new HashMap<>();
+        
+        // Jouer les parties et collecter les situations
+        for (int i = 0; i < nbParties; i++) {
+            Board board = new Board();
+            RandomAIPlayer player1 = new RandomAIPlayer(Disc.BLACK);
+            RandomAIPlayer player2 = new RandomAIPlayer(Disc.WHITE);
+            Player currentPlayer = player1;
+            
+            while (board.hasValidMoves(Disc.BLACK) || board.hasValidMoves(Disc.WHITE)) {
+                if (board.hasValidMoves(currentPlayer.getColor())) {
+                    Move move = currentPlayer.getMove(board);
+                    if (move != null) {
+                        board.makeMove(move);
+                        
+                        boolean isWinning = isWinningPosition(board, currentPlayer.getColor());
+                        String boardState = boardToString(board);
+                        
+                        situations.computeIfAbsent(boardState, BoardSituation::new)
+                                .addOccurrence(isWinning);
+                    }
+                }
+                currentPlayer = (currentPlayer == player1) ? player2 : player1;
+            }
+            
+            if ((i + 1) % 10 == 0) {
+                System.out.println("Parties analysées: " + (i + 1) + "/" + nbParties);
+            }
+        }
+        
+        // Écrire les situations uniques dans un nouveau fichier
+        String uniqueOutputPath = outputPath.replace(".csv", "_unique.csv");
+        try (FileWriter writer = new FileWriter(uniqueOutputPath)) {
+            for (BoardSituation situation : situations.values()) {
+                writer.write(situation.boardState);
+                writer.write(",");
+                writer.write(String.valueOf((double) situation.winCount / situation.totalCount));
+                writer.write(",");
+                writer.write(String.valueOf(situation.totalCount));
+                writer.write("\n");
+            }
+        } catch (IOException e) {
+            System.err.println("Erreur lors de l'écriture du fichier CSV: " + e.getMessage());
+        }
+
+        System.out.println("Export terminé! " + situations.size() + " situations uniques trouvées.");
+    }
+
+    /**
+     * Convertit un plateau en chaîne de caractères CSV
+     */
+    private String boardToString(Board board) {
+        StringBuilder sb = new StringBuilder();
+        Disc[][] grid = board.getGrid();
+        
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (grid[i][j] == Disc.BLACK) sb.append("1");
+                else if (grid[i][j] == Disc.WHITE) sb.append("-1");
+                else sb.append("0");
+                sb.append(",");
+            }
+        }
+        return sb.substring(0, sb.length() - 1); // Enlever la dernière virgule
+    }
+
+    /**
      * Point d'entrée pour lancer un export.
      * @param args Arguments non utilisés
      */
     public static void main(String[] args) {
         int nbParties = 100; // ou autre nombre souhaité
         MultipleGamesExporter exporter = new MultipleGamesExporter("training_data.csv", nbParties);
-        exporter.playAndExportGames();
+        exporter.playAndExportUniqueGames(); // Utilise la nouvelle fonction pour éviter les doublons
     }
 }
