5ffaffc32992c74ce601f59b8e6810cd518750ae
PalisseVolia <palissev@gmail.com>
2025-01-10 11:49:25 +0100

    play from db

--- projet/src/main/java/com/parallelai/exec/files/FilesUtils.java
+++ projet/src/main/java/com/parallelai/exec/files/FilesUtils.java
@@ -2,35 +2,29 @@ package com.parallelai.exec.files;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.stream.Stream;
 
 public class FilesUtils {
     
     public static void clearModelDirectories() throws IOException {
         String[] modelPaths = {
-            "projet/src/main/resources/models/CNN",
-            "projet/src/main/resources/models/MLP"
+            "projet\\src\\main\\ressources\\models\\CNN",
+            "projet\\src\\main\\ressources\\models\\MLP"
         };
         
         for (String modelPath : modelPaths) {
-            File directory = new File(modelPath);
-            if (directory.exists() && directory.isDirectory()) {
-                File[] files = directory.listFiles();
-                if (files != null) {
-                    for (File file : files) {
-                        if (file.isFile()) {
-                            file.delete();
-                        }
-                    }
+            Path path = Path.of(modelPath);
+            if (Files.exists(path)) {
+                try (Stream<Path> walk = Files.walk(path)) {
+                    walk.sorted(Comparator.reverseOrder())
+                        .map(Path::toFile)
+                        .filter(file -> !file.getPath().equals(path.toString()))
+                        .forEach(File::delete);
                 }
             }
-        }
-    }
-
-    public static void main(String[] args) {
-        try {
-            clearModelDirectories();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
